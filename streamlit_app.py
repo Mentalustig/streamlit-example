0627ccaef1b43b1a96cbb2232f6e2c5fbdade9f7
Mentalustig <46600733+Mentalustig@users.noreply.github.com>
2023-08-05 19:29:34 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,77 @@
 from collections import namedtuple
 import altair as alt
-import math
 import pandas as pd
 import streamlit as st
+import numpy as np
 
-"""
-# Welcome to Lucas
+# Hardcoded data
+data = {
+    'Week': ['2023-01-01', '2023-05-08'],
+    'Bank Account': [40000.0, 45000.0],
+    'Investment Account': [np.nan, 2000.0],
+    'Inheritance': [100000.0, 100000.0],
+    'House Dellach': [200000.0, 200000.0],
+    'Others': [5000.0, 5000.0]
+}
+df = pd.DataFrame(data)
+df['Week'] = pd.to_datetime(df['Week'])
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+# User input for new data
+def user_input_features():
+    account_types = ['Bank Account', 'Investment Account', 'Inheritance', 'House Dellach', 'Others']
+    account = st.selectbox('Account Type', account_types)
+    amount = st.number_input('Amount', value=0.0)
+    week = st.date_input('Week', value=datetime.now())
+    return week, account, amount
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+# Calculate goal progress
+def calculate_goal_progress(df, goal, interest_rate, years):
+    # Calculate future value of current holdings
+    future_value = df.sum().sum() * (1 + interest_rate)**years
+    # Calculate progress towards goal
+    goal_progress = future_value / goal
+    return goal_progress
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+if st.button('Add Data'):
+    week, account, amount = user_input_features()
+    df.loc[df['Week'] == week, account] = amount
 
+goal = st.number_input('Goal', value=0.0)
+interest_rate = st.number_input('Annual Interest Rate', value=0.0)
+years = st.number_input('Years', value=15)
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+goal_progress = calculate_goal_progress(df, goal, interest_rate, years)
+st.write('Goal Progress: ', goal_progress)
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+# Plot stacked area chart using Altair
+area_chart = alt.Chart(df.melt('Week', var_name='Account', value_name='Amount')).mark_area().encode(
+    x='Week:T',
+    y='Amount:Q',
+    color='Account:N',
+    tooltip=['Week', 'Account', 'Amount']
+)
+st.altair_chart(area_chart, use_container_width=True)
 
-    points_per_turn = total_points / num_turns
+# Display data table
+st.dataframe(df)
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+# Spiral plot
+total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
+num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+Point = namedtuple('Point', 'x y')
+spiral_data = []
+
+points_per_turn = total_points / num_turns
+
+for curr_point_num in range(total_points):
+    curr_turn, i = divmod(curr_point_num, points_per_turn)
+    angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
+    radius = curr_point_num / total_points
+    x = radius * math.cos(angle)
+    y = radius * math.sin(angle)
+    spiral_data.append(Point(x, y))
+
+st.altair_chart(alt.Chart(pd.DataFrame(spiral_data), height=500, width=500)
+    .mark_circle(color='#0068c9', opacity=0.5)
+    .encode(x='x:Q', y='y:Q'))

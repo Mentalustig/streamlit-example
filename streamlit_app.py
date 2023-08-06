0ee4737e1942bea7da2e6fd30a2ddb54f9759d31
Mentalustig <46600733+Mentalustig@users.noreply.github.com>
2023-08-06 10:00:07 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,9 +6,9 @@ import streamlit as st
 # Read in data from the Google Sheet.
 def load_data(sheets_url):
     csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
-    return pd.read_csv(csv_url, parse_dates=['Week'], thousands=',', decimal='.')
+    return pd.read_csv(csv_url)
 
 df = load_data(st.secrets["public_gsheets_url"])
 
-# Print results.
+# Print the entire DataFrame as a table.
 st.table(df)

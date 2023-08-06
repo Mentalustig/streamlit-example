780241db4f4ef2e497f827d356cef576fd9b2c74
Mentalustig <46600733+Mentalustig@users.noreply.github.com>
2023-08-06 14:44:47 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,9 @@ import streamlit as st
 import plotly.express as px
 from datetime import datetime
 
+# Constants
+GOAL = 800000
+
 # Read in data from the Google Sheet.
 @st.cache_data(ttl=600)
 def load_data(sheets_url):
@@ -10,9 +13,6 @@ def load_data(sheets_url):
     df = pd.read_csv(csv_url)
     df['Week'] = pd.to_datetime(df['Week'])
     return df
-
-# Constants
-GOAL = 800000
 
 # Load data
 df = load_data(st.secrets["public_gsheets_url"])
@@ -27,39 +27,40 @@ last_period_data = df.iloc[-2][['Bank Account', 'Investment Account', 'Inheritan
 
 # Stacked Bar chart for current vs last period
 stacked_bar_chart_data = pd.DataFrame({
-    'Category': ['Bank Account', 'Investment Account', 'Inheritance', 'House Dellach', 'Savings Account', 'Others'],
-    'Last Period': last_period_data,
-    'Current Period': current_data
-})
-st.bar_chart(stacked_bar_chart_data.set_index('Category'))
+    'Category': list(current_data.index),
+    'Last Period': last_period_data.values,
+    'Current Period': current_data.values
+}).melt(id_vars='Category', var_name='Period', value_name='Amount')
 
-# Calculate the difference between current and last period's sum
-current_sum = current_data.sum()
-last_period_sum = last_period_data.sum()
-difference = current_sum - last_period_sum
+fig_stacked_bar = px.bar(stacked_bar_chart_data, x='Period', y='Amount', color='Category', barmode='stack', title='Comparison of Last Period vs Current Period')
+st.plotly_chart(fig_stacked_bar)
 
-# Success message and balloons
-if difference >= 2000:
-    st.success(f"Congratulations! This month's money is {difference} more than last month's sum.")
-    st.balloons()
+# Print data as a table
+st.write(df)
 
 # Inputs (at the end)
 years_forecast = st.slider("Number of Years for Forecast", 1, 30, 5)
 monthly_investment_forecast = st.slider("Monthly Investment Forecast", 0, 6000, 2000)
-investment_interest_rate = st.slider("Investment Interest Rate (%)", 0, 10, 6) / 100
-house_dellach_interest_rate = st.slider("House Dellach Interest Rate (%)", 0, 10, 2) / 100
-savings_account_interest_rate = st.slider("Savings Account Interest Rate (%)", 0, 10, 4) / 100
+investment_interest_rate = st.slider("Investment Interest Rate (%)", 0, 10, 6)
+house_dellach_interest_rate = st.slider("House Dellach Interest Rate (%)", 0, 10, 2)
+savings_account_interest_rate = st.slider("Savings Account Interest Rate (%)", 0, 10, 4)
 
-# Forecast calculation
-forecasted_sum = current_data.sum() \
-                + current_data['House Dellach'] * (1 + house_dellach_interest_rate) ** years_forecast \
-                + current_data['Investment Account'] * (1 + investment_interest_rate) ** years_forecast \
-                + monthly_investment_forecast * 12 * (1 + investment_interest_rate) ** years_forecast \
-                + current_data['Savings Account'] * (1 + savings_account_interest_rate) ** years_forecast
+# Forecast calculations
+current_sum = current_data.sum()
+forecasted_sum = current_sum + \
+                 current_data['House Dellach'] * (1 + house_dellach_interest_rate / 100) ** years_forecast + \
+                 current_data['Investment Account'] * (1 + investment_interest_rate / 100) ** years_forecast + \
+                 monthly_investment_forecast * 12 * (1 + investment_interest_rate / 100) ** years_forecast + \
+                 current_data['Savings Account'] * (1 + savings_account_interest_rate / 100) ** years_forecast
 
-st.write(f"Forecasted Sum: {forecasted_sum}")
+# Donut Charts
+fig_donut_current = px.pie(values=[current_sum, GOAL - current_sum], names=['Current', 'Remaining'], hole=0.3, title='Current Goal Progress')
+st.plotly_chart(fig_donut_current)
+
+fig_donut_forecast = px.pie(values=[forecasted_sum, GOAL - forecasted_sum], names=['Forecasted', 'Remaining'], hole=0.3, title='Forecasted Goal Progress')
+st.plotly_chart(fig_donut_forecast)
 
 # Footnote with assumptions and current goal
 st.markdown("---")
 st.markdown("**Assumptions and Current Goal:**")
-st.markdown("The current goal is set at $800,000. This amount is based on the estimated monthly living expenses of $3,500 to $4,500. The forecast and visualizations above are built on the assumptions provided through the sliders, reflecting potential investment returns, interest rates, and other financial factors.")
+st.markdown("The current goal is set at $800,000. This amount is based on the estimated monthly living expenses of $3,500 to $4,500. The forecast and visualizations above are built on the assumptions provided through the sliders, reflecting potential investment returns, interest rates, and other financial factors

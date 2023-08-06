c939aaa6f56556482a8a3dd0a992000de831b4be
Mentalustig <46600733+Mentalustig@users.noreply.github.com>
2023-08-06 10:09:38 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,10 +6,9 @@ import streamlit as st
 # Title of the dashboard
 st.title("Mamis Finance Dashboard")
 
-# Button to direct to the Google Sheet
+# Hyperlink to direct to the Google Sheet
 google_sheet_url = "https://docs.google.com/spreadsheets/d/1MGyZNI0FjOSYc3SEh3ZTAcjPtipjNU_AAdtqUWzdBsU/edit#gid=0"
-if st.button("Aktualisiere deine Finanzdaten"):
-    st.markdown(f'<a href="{google_sheet_url}" target="_blank">Click here to open the Google Sheet</a>', unsafe_allow_html=True)
+st.markdown(f'[Open Google Sheet]({google_sheet_url})', unsafe_allow_html=True)
 
 # Read in data from the Google Sheet.
 # Uses st.cache_data to only rerun when the query changes or after 10 min.
@@ -19,6 +18,14 @@ def load_data(sheets_url):
     return pd.read_csv(csv_url)
 
 df = load_data(st.secrets["public_gsheets_url"])
+
+# Calculate the sum of money for the last two months
+last_month_sum = df.iloc[-2, 1:].replace('-', '0').replace('€', '', regex=True).replace(',', '', regex=True).astype(float).sum()
+this_month_sum = df.iloc[-1, 1:].replace('-', '0').replace('€', '', regex=True).replace(',', '', regex=True).astype(float).sum()
+
+# Check if this month's sum is at least 2000 more than last month's sum
+if this_month_sum - last_month_sum >= 2000:
+    st.success("Great! This month's total is at least 2000 more than last month's total! 🎉")
 
 # Print the entire DataFrame as a table.
 st.table(df)

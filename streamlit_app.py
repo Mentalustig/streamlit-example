52d2a442d3225311ac334dcf414cb9b84076a774
Mentalustig <46600733+Mentalustig@users.noreply.github.com>
2023-08-06 09:54:50 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,13 +4,12 @@ import pandas as pd
 import streamlit as st
 
 # Read in data from the Google Sheet.
-# Uses st.cache_data to only rerun when the query changes or after 10 min.
 def load_data(sheets_url):
     csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
-    return pd.read_csv(csv_url)
+    return pd.read_csv(csv_url, parse_dates=['Week'], thousands=',', decimal='.')
 
 df = load_data(st.secrets["public_gsheets_url"])
 
 # Print results.
 for row in df.itertuples():
-    st.write(f"{row.name}")
+    st.write(f"{row.Week}: Bank Account = {row._2}, Investment Account = {row._3}, Inheritance = {row._4}, House Dellach = {row._5}, Others = {row._6}")
